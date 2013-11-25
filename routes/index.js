d5495e906811d48c3f13e2635e7267c866464477
Nick Crohn <nick.crohn@gmail.com>
2013-11-24 18:15:42 -0700

    Switched to run on ec2

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,51 @@
-var DigitalOceanAPI = require('digitalocean-api');
+var AWS = require('aws-sdk');
 var keys = require('../keys.json');
 
-// Create an instance with your API credentials
-var api = new DigitalOceanAPI(keys.clientId, keys.apiKey);
+var ec2 = new AWS.EC2(keys);
+
+function getValue(hashes, key) {
+  for(var i=0;i<hashes.length;i++) {
+    if(hashes[i].Key === key) {
+      return hashes[i].Value;
+    }
+  }
+
+  return null;
+}
 
 exports.index = function(req, res){
 
-  api.dropletGetAll(function(error, droplets){
-    res.render('index', {droplets: droplets});
-  });
+  ec2.describeInstances({
+      Filters: [{
+        Name: 'tag-key',
+        Values: ['group']
+      },{
+        Name: 'tag-value',
+        Values: ['minecraft']
+      }]
+    }, function(error, data){
+      if (error) { return res.send(500, error); }
+
+      var instances = [];
+      for(var i=0;i<data.Reservations.length;i++) {
+        var reservation = data.Reservations[i];
+
+        for(var j=0;j<reservation.Instances.length;j++) {
+          var inst = reservation.Instances[j];
+
+          var instance = {
+            id: inst.InstanceId,
+            name: getValue(inst.Tags, 'Name'),
+            ip_address: inst.PublicIpAddress,
+            status: inst.State.Name
+          };
+
+          instances.push(instance);
+        }
+      }
+
+      return res.render('index', {instances: instances});
+    });
 
 };
 
@@ -16,7 +53,9 @@ exports.start = function(req, res) {
 
   var serverId = req.params.serverId;
 
-  api.dropletPowerOn(serverId, function(error, data){
+  ec2.startInstances({
+    InstanceIds: [serverId]
+  }, function(error, data){
     if (error) {
       res.send(500, error);
     } else {
